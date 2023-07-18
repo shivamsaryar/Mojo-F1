dc88d74ef6ac33d6a19d818c8eaf896bbd994e95
Shivam Saryar <shivamsaryar@gmail.com>
2023-07-18 16:42:50 +1000

    rename folder

--- bigquery-python/bigquery-google-sheets/connect-to-gsheets.py
+++ bigquery-python/bigquery-google-sheets/connect-to-gsheets.py
@@ -4,7 +4,7 @@ import google.auth
 
 # Create credentials with Drive & BigQuery API scopes.
 # Both APIs must be enabled for your project before running this code.
-credentials, project = google.auth. default (
+credentials, project = google.auth.default (
     scopes=[
         "https: //www.googleapis.com/auth/drive",
         "https://www.googleapis.com/auth/bigquery"
